f619990e06d17d0315d2ed3d63e8b6db2ee64da2
MCarr0317 <macdd9@mst.edu>
2014-10-21 14:58:35 -0700

    Create Parser.py
    Function to take a single SELECT statement (not nested) and parse it into its attributes, relations, and conditional sections.

---
+++ SQL-Optimizer/Parser.py
@@ -0,0 +1,62 @@
+
+class Parser:
+    def __init__(self, sql):
+        self.attributes = []
+        self.relations = []
+        self.conditionals = []
+        self.sql = sql
+        self.parsed = None
+
+    def parse(self):
+        self.parsed = self.sql.split()
+        self.parsed = [x.strip(',;()') for x in self.parsed]
+        self.extract_attributes()
+        self.extract_relations()
+        self.extract_conditionals()
+
+    def extract_attributes(self):
+        from_reached = False
+        for token in self.parsed:
+            if token == 'FROM':
+                from_reached = True
+
+            if token != 'SELECT' and token != 'FROM':
+                if not from_reached:
+                    self.attributes.append(token)
+
+    def extract_relations(self):
+        from_reached = False
+        for token in self.parsed:
+            if token == 'FROM':
+                from_reached = True
+                continue
+            elif token == 'WHERE':
+                break
+            if from_reached:
+                self.relations.append(token)
+
+    def extract_conditionals(self):
+        where_reached = False
+        for token in self.parsed:
+            if token == 'WHERE':
+                where_reached = True
+                continue
+            if where_reached:
+                self.conditionals.append(token)
+
+
+    def output(self):
+        print '\nStatement to parse: ', self.sql
+        print '\tTokens: ', self.parsed
+        print '\tAttributes: ', self.attributes
+        print '\tRelations: ', self.relations
+        print '\tConditions: ', self.conditionals, '\n'
+        
+        
+        
+        
+        
+        
+test_parsed = Parser(test[0])
+test_parsed.parse()
+test_parsed.output()
