f6a63be8785e326a60d9819586e93b59ebafbdad
MCarr0317 <macdd9@mst.edu>
2014-10-29 15:15:44 -0500

    Update Translator.py

--- SQL-Optimizer/Translator.py
+++ SQL-Optimizer/Translator.py
@@ -1,9 +1,12 @@
 __author__ = 'Matthew Carr'
 #translates SQL into relational algebra
+from copy import deepcopy
+
+keywords = {'SELECT': 'PROJECT', 'FROM': '(', 'WHERE': 'SELECT', ')': '', 'JOIN': 'JOIN'}
 
 
 def translate_tokens(parsed_statement):
-    keywords = {'SELECT': 'PROJECT', 'FROM': '(', 'WHERE': 'SELECT'}
+    #keywords = {'SELECT': 'PROJECT', 'FROM': '(', 'WHERE': 'SELECT'}
     list_ = []
     for token in parsed_statement:
         if token in keywords:
@@ -12,7 +15,8 @@ def translate_tokens(parsed_statement):
             list_.append(token)
 
     list_.append(')')
-    return list_
+    return ' '.join(insert_tokens(list_))
+
 
 
 def get_attributes(translated_statement):
@@ -26,38 +30,43 @@ def get_attributes(translated_statement):
 
     return translated_statement[x:y]
 
-def get_relations(translated_statement):
+
+def get_relation_tokens(translated_statement):  # returns relation tokens without parenthesis
     x, y = 0, 0
     for i in xrange(len(translated_statement)):
         if translated_statement[i] == '(':
-            x = i+1
+            x = i
         elif translated_statement[i] == ')':
             y = i
 
-    return translated_statement[x:y]
+    return translated_statement[x+1:y], x
+
+
+def get_relations(relation_tokens):  # return only relation table names including parenthesis
+    relations = []
+    for i in xrange(1, len(relation_tokens)):
+        if relation_tokens[i] not in keywords:
+            relations.append(relation_tokens[i])
+    return relations
 
 
 def insert_tokens(translated_statement):
-    relations = []
-    x, y = 0, 0
-    for i in xrange(len(translated_statement)):
-        if translated_statement[i] == '(':
-            x = i+2
-        if x and translated_statement[i] == ')':
-            y = i
-        if y:
-            relations = translated_statement[x:y+1]
+    relation_tokens, x = get_relation_tokens(translated_statement)
+    relations = get_relations(relation_tokens)
+    temp = deepcopy(relation_tokens)
 
-    if not 'JOIN' in relations:
-        for i in xrange(0, len(relations), 2):
-            relations.insert(i, 'X')
+    print get_attributes(translated_statement)
+    #if not 'JOIN' in relations:
+    for i in xrange(1, len(relation_tokens)):
+        if relation_tokens[i] != 'JOIN' and relation_tokens[i] not in relations:
+            temp.insert(i, 'X')
+        elif relation_tokens[i] == 'JOIN':
+            temp.insert(i-1, '(')
+            temp.insert(i+3, ')')
 
-    return translated_statement[:x] + relations
-
+    return translated_statement[:x] + temp
 
 
+list_ = ['SELECT', 'studentID', 'Name', 'GPA', 'FROM', 'student', 'WHERE', 'GPA', '>', '3.0']
 
-list = ['SELECT', 'studentID', 'Name', 'GPA', 'FROM', 'student', 'student2']
-
-print ' '.join(insert_tokens((translate_tokens(list))))
-
+print translate_tokens(list_)
