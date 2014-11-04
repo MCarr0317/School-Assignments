70e2163fbda7ef91e267ecc0b1386f726b366f76
MCarr0317 <macdd9@mst.edu>
2014-11-03 21:04:25 -0600

    Update main.py

--- CS5300/SQL-Optimizer/main.py
+++ CS5300/SQL-Optimizer/main.py
@@ -1,7 +1,7 @@
 __author__ = 'Matthew Carr'
 
 from Query_Objects import *
-
+from Query_tree import *
 
 
 def main():
@@ -9,6 +9,8 @@ def main():
     test_cases = open('test_cases.txt', 'r')
 
     for test in test_cases:
+        table_list = []
+        attribute_list = []
         statements = Translator.separate_statements(test)
         #print statements
 
@@ -16,6 +18,25 @@ def main():
             T = Translator(statement)
             print T
             print
+            tree = QueryTree()
+            for i in xrange(1, len(T.Parsed.from_clause)):
+                table_list.append(tree.Node(T.Parsed.from_clause[i]))
+
+            for i in xrange(1, len(T.Parsed.select_clause)):
+                attribute_list.append((T.Parsed.select_clause[i]))
+
+            if len(table_list) > 1:
+                join = tree.Node('X', table_list)
+                select = tree.Node(' '.join(T.Parsed.where_clause), [join])
+                project = tree.Node(' '.join(T.Parsed.select_clause), [select])
+            else:
+                select = tree.Node(' '.join(T.Parsed.where_clause), table_list)
+                project = tree.Node(' '.join(T.Parsed.select_clause), [select])
+
+            tree.output()
+            print
+            
+        print '-'*80
 
 
 if __name__ == "__main__":
