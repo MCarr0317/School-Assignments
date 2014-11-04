038b32bcf4e9cc76096a9a9cbc091f297ac34f7c
MCarr0317 <macdd9@mst.edu>
2014-11-03 21:33:52 -0600

    Update main.py

--- CS5300/SQL-Optimizer/main.py
+++ CS5300/SQL-Optimizer/main.py
@@ -9,33 +9,26 @@ def main():
     test_cases = open('test_cases.txt', 'r')
 
     for test in test_cases:
-        table_list = []
-        attribute_list = []
+        print test
         statements = Translator.separate_statements(test)
-        #print statements
 
         for statement in statements:
             T = Translator(statement)
-            print T
-            print
+            T.Translate()
+            print T, '\n'
             tree = QueryTree()
-            for i in xrange(1, len(T.Parsed.from_clause)):
-                table_list.append(tree.Node(T.Parsed.from_clause[i]))
 
-            for i in xrange(1, len(T.Parsed.select_clause)):
-                attribute_list.append((T.Parsed.select_clause[i]))
-
-            if len(table_list) > 1:
-                join = tree.Node('X', table_list)
+            if len(T.Parsed.from_clause[1:]) > 1:
+                join = tree.Node('X', [tree.Node(x) for x in T.Parsed.from_clause[1:]])
                 select = tree.Node(' '.join(T.Parsed.where_clause), [join])
                 project = tree.Node(' '.join(T.Parsed.select_clause), [select])
             else:
-                select = tree.Node(' '.join(T.Parsed.where_clause), table_list)
+                select = tree.Node(' '.join(T.Parsed.where_clause), [tree.Node(i) for i in T.Parsed.from_clause[1:]])
                 project = tree.Node(' '.join(T.Parsed.select_clause), [select])
 
             tree.output()
             print
-            
+
         print '-'*80
 
 
